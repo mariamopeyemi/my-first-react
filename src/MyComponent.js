96a66c345ecd8aff1a2903bb9c4b2abd5e0e9a5e
mariamopeyemi <opeyemi.afolabi45@gmail.com>
2021-10-13 06:32:48 +0100

    my first react app

---
+++ src/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+const MyComponent = ()=>{
+    return(
+        <div>
+            <h1>My name is Opeyemi Mariam</h1>
+            <h4>I am 24 years old</h4>
+            <h4>I am from Osun state</h4>
+            <h4>I am a pet mom</h4>
+        </div>
+    );
+};
+
+export default MyComponent;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyComponent from './MyComponent';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
